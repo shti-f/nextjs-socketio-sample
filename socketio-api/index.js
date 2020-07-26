43d367b4150f0aa60bd8a68109bb425b04ca3947
shti-f <shtif.dev@gmail.com>
2020-07-26 12:52:02 +0900

    initial commit

---
+++ socketio-api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 4001;
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
